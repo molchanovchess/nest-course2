7557e049e928336e9dde8fcdca56333f9576f3ed
alexmolchanov <oleksiimolchanov@bitbucket.org>
2024-07-27 12:38:07 +0300

    REST API (prisma + neon postgres16)

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { GroceriesModule } from './groceries/groceries.module';
 import { PostsModule } from './posts/posts.module';
+import { DatabaseModule } from './database/database.module';
+import { EmployeesModule } from './employees/employees.module';
 
 @Module({
-  imports: [UsersModule, GroceriesModule, PostsModule],
+  imports: [UsersModule, GroceriesModule, PostsModule, DatabaseModule, EmployeesModule],
   controllers: [AppController],
   providers: [AppService],
 })
