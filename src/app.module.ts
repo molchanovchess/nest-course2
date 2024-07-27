df46fe81dfd19f62ec04a07baa744bf77e8d0654
alexmolchanov <oleksiimolchanov@bitbucket.org>
2024-07-27 15:59:27 +0300

    essential additions

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,38 @@ import { GroceriesModule } from './groceries/groceries.module';
 import { PostsModule } from './posts/posts.module';
 import { DatabaseModule } from './database/database.module';
 import { EmployeesModule } from './employees/employees.module';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+import { MyLoggerModule } from './my-logger/my-logger.module';
 
 @Module({
-  imports: [UsersModule, GroceriesModule, PostsModule, DatabaseModule, EmployeesModule],
+  imports: [
+    UsersModule,
+    GroceriesModule,
+    PostsModule,
+    DatabaseModule,
+    EmployeesModule,
+    ThrottlerModule.forRoot([
+      {
+        name: 'short',
+        ttl: 1000,
+        limit: 3,
+      },
+      {
+        name: 'long',
+        ttl: 60000,
+        limit: 100,
+      },
+    ]),
+    MyLoggerModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, HttpAdapterHost } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import { AllExceptionsFilter } from './all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
 
   // Define your CORS options
   const corsOptions: CorsOptions = {
@@ -15,7 +19,7 @@ async function bootstrap() {
 
   // Enable CORS with the options
   app.enableCors(corsOptions);
-
+  app.setGlobalPrefix('api');
   await app.listen(5000);
 }
 bootstrap();
